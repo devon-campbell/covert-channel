15c3edbd46c758921d2876350f37f69e8b6b88bf
matheucampbell <matheu.campbell.us@outlook.com>
2025-04-07 19:23:49 -0400

    send RAS refactor

--- ras/arq_ras/send.c
+++ ras/arq_ras/send.c
@@ -1,36 +1,37 @@
 #include "send.h"
 #include "print_utils.h"
+#include <sched.h>
+
+#define RAS_SIZE 16
+
+static inline void flush_ras(int count, int threshold){
+    if (count == threshold) {
+        sched_yield();
+        return;
+    }
+    else flush_ras(++count, threshold);
+}
 
 // Send either high or low for a given number of cycles
-inline void send_bit(void *target_address, bool bit)
-{
-
+static inline void send_bit(bool bit){
     // Wait until time step A
     uint32_t initial = start_sync();
 
-    if (bit)
-    {
+    if (bit){
         // Send until time step B
-        while (!is_half_point())
-        {
-            flush_event((uint64_t)target_address);
+        while (!is_half_point()){
+            flush_ras(0, RAS_SIZE);
         }
-    }
-    else
-    {
+    }else{
         while (!is_half_point())
-            ;
+                ;
     }
-
- //   print_bit(bit, "Sent: ");
 }
 
 // Send a stream of bits
-inline void send_bits(bool *bits, size_t num_bits, void *target_address)
-{
-    for (size_t i = 0; i < num_bits; i++)
-    {
-        send_bit(target_address, bits[i]);
+inline void send_bits(bool *bits, size_t num_bits){
+    for (size_t i = 0; i < num_bits; i++)    {
+        send_bit(bits[i]);
     }
 }
 
@@ -42,11 +43,11 @@ inline void send_byte_frame(void *target_address, uint8_t byte)
     struct frame frame = construct_frame(byte);
 
     // Send start delimiter
-    send_bits(frame.start_delimiter, sizeof(frame.start_delimiter), target_address);
+    send_bits(frame.start_delimiter, sizeof(frame.start_delimiter));
     // Send data
-    send_bits(frame.data, sizeof(frame.data), target_address);
+    send_bits(frame.data, sizeof(frame.data));
     // Send parity
-    send_bit(target_address, frame.parity);
+    send_bit(frame.parity);
 
     // // Print the frame for debugging
     // print_frame(frame);
